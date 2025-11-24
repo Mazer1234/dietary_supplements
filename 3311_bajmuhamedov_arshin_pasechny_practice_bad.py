cabcc7f99e76edd151b4fb7768f2dcd06b8dfa2c
Rafael <165057940+brick1ng5654@users.noreply.github.com>
2025-11-24 22:09:09 +0300

    Fix notebook last cell step (#42)
    Исправлена логика в сохранении изменений, для того чтобы джобы run-notebook и lint не падали

--- 3311_bajmuhamedov_arshin_pasechny_practice_bad.py
+++ 3311_bajmuhamedov_arshin_pasechny_practice_bad.py
@@ -857,12 +857,17 @@ print_info(df)
 # %% [markdown]
 # # Сохранение изменений
 
-# %%
-
 # %% tags=["skip"]
-import os
+import os, sys
 import pathlib
 from pathlib import Path
+
+try:
+    _system = get_ipython().system
+except NameError:
+    import subprocess
+    def _system(cmd):
+        return subprocess.check_call(cmd, shell=True)
 
 IN_COLAB = False
 try:
@@ -871,22 +876,30 @@ try:
 except ImportError:
     IN_COLAB = False
 
-if IN_COLAB:
+if not IN_COLAB:
+    print("Окружение не Colab. Настройка на безопасную версию")
+    NOTEBOOK = "3311_bajmuhamedov_arshin_pasechny_practice_bad.ipynb"
+    cfg_path = ".jupytext.toml"
+else:
     print("Running in Colab, executing Jupytext sync logic...")
     
-    !pip -q install jupytext nbstripout
     from google.colab import drive
     drive.mount('/content/drive')
     
     NOTEBOOK = "/content/drive/MyDrive/Colab Notebooks/3311_bajmuhamedov_arshin_pasechny_practice_bad.ipynb"
+    cfg_path = "/content/.jupytext.toml"
     
-    cfg = '''formats = "ipynb,py:percent"
-    cell_metadata_filter = "-all,tags"
-    notebook_metadata_filter = "kernelspec,jupytext"
-    '''
-    with open("/content/.jupytext.toml", "w", encoding="utf-8") as f:
-        f.write(cfg)
-        
+_system(f'pip -q install jupytext nbstripout')
+_system(f'nbstripout "{NOTEBOOK}"')
+
+cfg = '''formats = "ipynb,py:percent"
+cell_metadata_filter = "-all,tags"
+notebook_metadata_filter = "kernelspec,jupytext"
+'''
+with open(cfg_path, "w", encoding="utf-8") as f:
+    f.write(cfg)
+
+if IN_COLAB:    
     ipynb_path = Path(NOTEBOOK)
     py_path = ipynb_path.with_suffix(".py")
 
@@ -895,17 +908,17 @@ if IN_COLAB:
 
     print("IPYNB:", ipynb_path)
     print("PY:", py_path)
-
-    !nbstripout "{NOTEBOOK}"
-
+    
+    _system(f'nbstripout "{NOTEBOOK}"')
+    
     if py_path.exists():
         py_path.unlink()
-    !jupytext --to py:percent "{NOTEBOOK}"
+    
+    _system(f'jupytext --to py:percent "{NOTEBOOK}"') 
 
     import datetime
     stat = py_path.stat()
     print("\nОбновлён .py:", py_path)
-
+    
 else:
-    print("Environment is NOT Colab (GitHub Actions detected). Skipping sync logic.")
     pass
